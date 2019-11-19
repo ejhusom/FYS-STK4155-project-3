8a847de438106e3b980a8583127d08ec8d969045
ejhusom <ejlgkvam@online.no>
2019-11-19 11:58:35 +0100

    Ramblin

--- ej/src/preprocess_rain_data.py
+++ ej/src/preprocess_rain_data.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 # ============================================================================
-# File:     test_DecisionTreeRegressor.py
+# File:     preprocess_rain_data.py
 # Author:   Erik Johannes Husom
 # Created:  2019-11-14
 # ----------------------------------------------------------------------------
 # Description:
 #
 # ============================================================================
+import numpy as np
 
 
-def generate_data():
-    """Generate random data set to be used for regression testing."""
+def load_data():
+    filename = 'data/d.csv'
+    data = np.loadtxt(open(filename, 'rb'), delimiter=',', skiprows=1)
+    print(data)
 
-    X = np.linspace(-3, 3, n).reshape(-1, 1)
-    y = np.exp(-x**2) + 1.5 * np.exp(-(x-2)**2)+ np.random.normal(0, 0.1, x.shape)
 
-    return X, y
-
+if __name__ == '__main__': 
+    load_data()
