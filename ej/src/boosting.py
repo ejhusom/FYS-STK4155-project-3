63a3cd5fbeb4d5c3c3c7fdc7d8f28b826a9ce438
ejhusom <ejlgkvam@online.no>
2019-11-20 15:38:44 +0100

    Restructure

--- ej/src/boosting.py
+++ ej/src/boosting.py
@@ -21,6 +21,62 @@ import time
 
 from activity_data import *
 
+class Boost():
+    """Wrapper class for Scikit-Learn's boosting methods."""
+
+    def __init__(self, 
+            method='adaboost',
+            seed=0,
+            n_estimators=100,
+            learning_rate=0.5,
+            max_depth=3,
+            verbose=True):
+
+        self.method = method
+        self.seed = seed
+        self.n_estimators = n_estimators
+        self.learning_rate = learning_rate
+        self.max_depth = max_depth
+        self.verbose = verbose
+
+        if method == 'adaboost':
+            self.adabooster()
+        elif method == 'gradientboost':
+            self.gradientbooster()
+        elif method == 'xgboost':
+            self.xgbooster()
+        else:
+            print('Provide boost method.')
+            sys.exit(1)
+
+        print('--------------------------------------------'
+        print(f'Boost method: {self.method}')
+
+
+    def adabooster(self, base_estimator=DecisionTreeClassifier()):
+
+        self.clf = AdaBoostClassifier(
+                base_estimator=base_estimator,
+                n_estimators=self.n_estimators, 
+                algorithm='SAMME.R', 
+                learning_rate=self.learning_rate,
+                random_state=self.seed)
+        
+    
+    def gradientbooster(self):
+
+        self.clf = GradientBoostingClassifier(
+                max_depth=self.max_depth, 
+                n_estimators=self.n_estimators,
+                learning_rate=self.learning_rate)
+
+
+
+    def xgbooster(self):
+        
+        self.clf = xgb.XGBClassifier()
+
+
 def scale_data(train_data, test_data, scaler='standard'):
     """Scale train and test data.
 
@@ -74,56 +130,53 @@ def plot_confusion_matrix(y_test, y_pred, analysis_id=None):
     plt.show()
 
 
+
 class AnalyzeBoost():
-    """Wrapper class for Scikit-Learn's boosting methods."""
 
     def __init__(self, 
-            X_train, X_test, y_train, y_test,
-            method='adaboost',
-            seed=0,
-            n_estimators=100,
-            learning_rate=0.5,
-            max_depth=3,
+            scale=True, 
+            method='xgboost',
             verbose=True):
 
-        self.X_train = X_train
-        self.X_test = X_test
-        self.y_train = y_train
-        self.y_test = y_test
+
+        self.data = ActivityData(dirname='data/activity/', subjects=[1,2,3])
+        self.X, self.y = data.get_matrices()
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.2)
+
+        if scale:
+            self.X_train, self.X_test = scale_data(X_train, X_test, scaler='standard')
 
         self.method = method
-        self.seed = seed
-        self.n_estimators = n_estimators
-        self.learning_rate = learning_rate
-        self.max_depth = max_depth
         self.verbose = verbose
 
-        if method == 'adaboost':
+        if self.verbose:
+            print('-----------------------')
+            print(f'Number of training samples: {np.shape(self.X_train)[0]}')
+            print(f'Number of test samples: {np.shape(self.X_test)[0]}')
+            print(f'Scaled: {scale}')
+            print(f'Method: {method}')
+
+
+
+    def analyze(self):
+        if self.method == 'adaboost':
             self.adabooster()
-        elif method == 'gradientboost':
+        elif self.method == 'gradientboost':
             self.gradientbooster()
-        elif method == 'xgboost':
+        elif self.method == 'xgboost':
             self.xgbooster()
         else:
             print('Provide boost method.')
             sys.exit(1)
 
-        print('--------------------------------------------'
-        print(f'Boost method: {self.method}')
 
-
-    def boost_analysis():
-        np.random.seed(2020)
-
-
-        boost = Boost(method='xgboost')
-        boost.clf.fit(X_train, y_train)
-        accuracy = cross_validate(boost.clf, X_test, y_test, cv=10)['test_score']
+        self.clf.fit(self.X_train, self.y_train)
+        accuracy = cross_validate(self.clf, self.X_test, self.y_test, cv=10)['test_score']
         print(accuracy)
 
-        y_pred = boost.clf.predict(X_test)
+        y_pred = boost.clf.predict(self.X_test)
 
-        plot_confusion_matrix(y_test, y_pred)
+        plot_confusion_matrix(self.y_test, self.y_pred)
 
 
     def adabooster(self, base_estimator=DecisionTreeClassifier()):
@@ -149,11 +202,6 @@ class AnalyzeBoost():
         
         self.clf = xgb.XGBClassifier()
 
-
-
 if __name__ == '__main__':
-    data = ActivityData(dirname='data/activity/', subjects=[1,2,3])
-    X, y = data.get_matrices()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    X_train, X_test = scale_data(X_train, X_test, scaler='standard')
-    analysis = AnalyzeBoost(X_train, X_test, y_train, y_test)
+    np.random.seed(2020)
+    analysis = AnalyzeBoost()
