2b36fa6804fc70b7e1dba0e6968d521127b55c65
ejhusom <ejlgkvam@online.no>
2019-11-22 13:50:46 +0100

    Better structure of class, add comments

--- ejhusom/src/boosting.py
+++ ejhusom/src/boosting.py
@@ -72,10 +72,23 @@ def scale_data(train_data, test_data, scaler='standard'):
 
 
 def plot_confusion_matrix(y_test, y_pred, analysis_id=None):
-    """Plotting confusion matrix of a classification model."""
+    """Plotting confusion matrix of a classification model.
+    
+    Parameters
+    ----------
+    y_test : array
+        Target values from the test set.
+    y_pred : array
+        Predicted targets of the test set.
+    analysis_id : str, default=None
+        ID to be used when saving the plot. If None, a timestamp is used.
+        
+    """
 
     ax = skplt.metrics.plot_confusion_matrix(y_test, y_pred, normalize=True, cmap='Blues',
             title=' ')
+
+    # Fixing cropped top and bottom
     bottom, top = ax.get_ylim()
     ax.set_ylim(bottom + 0.5, top - 0.5)
 
@@ -86,9 +99,12 @@ def plot_confusion_matrix(y_test, y_pred, analysis_id=None):
 
     plt.show()
 
+
 def report(results, n_top=3):
-    """Utility function from Scikit-Learn's documentation.
-    scikit-learn.org/stable/auto_examples/model_selection/plot_randomized_search
+    """Utility function for printing results from grid search.
+    
+    Taken from Scikit-Learn's documentation:
+    https://scikit-learn.org/stable/auto_examples/model_selection/plot_randomized_search
     """
 
     for i in range(1, n_top + 1):
@@ -103,11 +119,42 @@ def report(results, n_top=3):
 
 
 class AnalyzeBoost():
+    """Analyzing the performance of three different boosting methods:
+    - AdaBoost,
+    - Gradient boost,
+    - XGBoost.
+
+    Parameters
+    ----------
+    X_train : array
+      Features of the training set.
+    X_test : array
+      Features of the test set.
+    y_train : array
+      Targets of the training set.
+    y_test : array
+      Targets of the test set.
+    method : str
+        Boosting method to analyze.
+    seed : float
+        Random seed.
+    n_estimators : int
+    learning_rate : float
+    max_depth : int
+    verbose : boolean
+        If True, printouts from the process are provided.
+
+    Attributes
+    ----------
+    attribute : float
+       Description.
+
+
+    """
 
     def __init__(self, 
             X_train, X_test, y_train, y_test,
             method='xgboost',
-            search_method=None,
             seed=0,
             n_estimators=100,
             learning_rate=0.5,
@@ -121,7 +168,6 @@ class AnalyzeBoost():
         self.y_test = y_test
 
         self.method = method
-        self.search_method = search_method
         self.seed = seed
         self.n_estimators = n_estimators
         self.learning_rate = learning_rate
@@ -139,13 +185,14 @@ class AnalyzeBoost():
 
 
         if self.method == 'adaboost':
-            self.adabooster()
+            self.base_estimator = DecisionTreeClassifier()
+            self.clf = AdaBoostClassifier(base_estimator=self.base_estimator)
             self.max_depth_str = 'base_estimator__max_depth'
         elif self.method == 'gradientboost':
-            self.gradientbooster()
+            self.clf = GradientBoostingClassifier()
             self.max_depth_str = 'max_depth'
         elif self.method == 'xgboost':
-            self.xgbooster()
+            self.clf = xgb.XGBClassifier()
             self.max_depth_str = 'max_depth'
         else:
             print('Provide boost method.')
@@ -154,120 +201,107 @@ class AnalyzeBoost():
 
 
     def fit(self):
+        parameters = {
+                'n_estimators': self.n_estimators, 
+                'learning_rate': self.learning_rate,
+                self.max_depth_str: self.max_depth}
 
-        if self.search_method == 'grid':
-            self.gridsearch()
-        # elif self.search_method == 'random':
-        #     self.randomsearch()
-        else:
-            parameters = {
-                    'n_estimators': self.n_estimators, 
-                    'learning_rate': self.learning_rate,
-                    self.max_depth_str: self.max_depth}
+        self.clf.set_params(**parameters)
+        self.clf.fit(self.X_train, self.y_train)
 
-            self.clf.set_params(**parameters)
-            self.clf.fit(self.X_train, self.y_train)
-
-            # Save model
-            pickle.dump(self.clf, open(
-                self.time_id + '-' + self.method'-fit.pkl', 'wb'))
+        # Save model
+        pickle.dump(self.clf, open(
+            self.time_id + '-' + self.method + '-fit.pkl', 'wb'))
             
 
 
     def predict(self):
 
-        if self.search_method == 'grid':
-            pass
-        # elif self.search_method == 'random':
-        #     pass
-        else:
-            self.y_pred = self.clf.predict(self.X_test)
-            accuracy = accuracy_score(self.y_pred, self.y_test)
-            print(f'Test accuracy score: {np.around(accuracy, decimals=3)}')
+        self.y_pred = self.clf.predict(self.X_test)
+        accuracy = accuracy_score(self.y_pred, self.y_test)
+        print(f'Test accuracy score: {np.around(accuracy, decimals=3)}')
 
-            plot_confusion_matrix(self.y_test, self.y_pred,
-                    analysis_id=self.time_id)
+        plot_confusion_matrix(self.y_test, self.y_pred,
+                analysis_id=self.time_id)
 
 
-    def gridsearch(self, parameters=None, cv=2):
+    def gridsearch(self, parameters=None, cv=2, load_search=None):
+        """Performing a grid search for optimal parameters.
+
+        Parameters
+        ----------
+        parameters : dict
+            Dictionary with the parameters to be tested in the grid search.
+        cv : int
+            Number of folds in the cross-validation.
+        load_search : pickle dump
+            The search model from a potential previous grid search, to avoid
+            doing a new grid search. If None, a new grid search is performed.
+
+
+        """
         
-        if parameters is None:
-            parameters =  [
-                    {'learning_rate': [1, 0.5, 0.1, 0.05, 0.01],
-                     'n_estimators': [50, 100, 150, 200],
-                     self.max_depth_str: [5, 7, 9]}
-            ]
+        if load_search is None:
+            if parameters is None:
+                parameters =  [
+                        {'learning_rate': [1, 0.5],
+                         'n_estimators': [100, 150, 200],
+                         self.max_depth_str: [5, 7]}
+                ]
 
-        self.search = GridSearchCV(self.clf, param_grid=parameters, cv=cv,
-                n_jobs=3)
-        self.search.fit(X_train, y_train)
+            self.search = GridSearchCV(self.clf, param_grid=parameters, cv=cv,
+                    n_jobs=3)
+            self.search.fit(X_train, y_train)
 
-        # Save model
-        pickle.dump(self.search, open(
-            self.time_id + '-' + self.method'-search.pkl', 'wb'))
+            # Save model
+            pickle.dump(self.search, open(
+                self.time_id + '-' + self.method + '-search.pkl', 'wb'))
+
+        else:
+            self.search = pickle.load(open(load_search, 'rb'))
 
         # Save results from grid search and print to terminal
         cv_results = pd.DataFrame(self.search.cv_results_) 
         cv_results.to_csv(f'{self.time_id}-gridsearch.csv')
         report(self.search.cv_results_)
 
-        self.best_learning_rate = self.search.best_params_['learning_rate']
-        self.best_n_estimators = self.search.best_params_['n_estimators']
-        self.best_max_depth = self.search.best_params_[self.max_depth_str]
-
-    
-    # def randomsearch(self, parameters=None, n_iter=10, cv=10):
-        
-        # TODO: Implement randomized search. The commented code does not work.
-        # if parameters is None:
-        #     parameters =  [
-        #             {'learning_rate': [1, 0.5, 0.1],#, 0.05, 0.01],
-        #              'n_estimators': sp_randint(10,100)}#, 100, 150]}
-        #     ]
-        #     if self.method != 'adaboost':
-        #         parameters['max_depth']: [3, 5, 7, 9]
-
-        # self.search = RandomizedSearchCV(self.clf,
-        #         param_distributions=parameters, n_iter=n_iter, cv=cv)
-        # self.search.fit(X_train, y_train)
-        # report(self.search.cv_results_)
+        # Overwriting parameters to the best parameters found by search
+        self.learning_rate = self.search.best_params_['learning_rate']
+        self.n_estimators = self.search.best_params_['n_estimators']
+        self.max_depth = self.search.best_params_[self.max_depth_str]
 
 
-
-    def adabooster(self):
-
-        self.base_estimator = DecisionTreeClassifier()
-        self.clf = AdaBoostClassifier(base_estimator=self.base_estimator)
-
-
-    def gradientbooster(self):
-
-        self.clf = GradientBoostingClassifier()
-
-
-    def xgbooster(self):
-        
-        self.clf = xgb.XGBClassifier()
 
 
 if __name__ == '__main__':
     np.random.seed(2020)
 
-    if not os.path.exists('activity_data_preprocessed.npy'):
-        data = ActivityData(dir='data/activity/', n_files=1)
+    data_file = 'activity_data_preprocessed.npy'
+
+    # Preprocess data if not already done
+    if not os.path.exists(data_file):
+        data = ActivityData(dir='data/activity/', n_files=15)
         data.output_to_npy()
 
-    data = np.load('activity_data_preprocessed.npy')
+    data = np.load(data_file)
     X = data[:,:-1]
     y = data[:,-1]
 
+    # Split and scale
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     X_train, X_test = scale_data(X_train, X_test, scaler='standard')
 
-    # analysis = AnalyzeBoost(X_train, X_test, y_train, y_test,
-    #         method='adaboost',
-    #         search_method='grid')
+    analysis = AnalyzeBoost(X_train, X_test, y_train, y_test,
+            method='xgboost',
+            n_estimators=150,
+            learning_rate=0.5,
+            max_depth=5)
 
-    analysis = AnalyzeBoost(X_train, X_test, y_train, y_test, method='xgboost')
+    # analysis.gridsearch()
+    analysis.fit()
+    analysis.predict()
+
+    # analysis = AnalyzeBoost(X_train, X_test, y_train, y_test, method='xgboost')
+    # analysis.fit()
     # analysis = AnalyzeBoost(X_train, X_test, y_train, y_test, method='gradientboost')
     # analysis = AnalyzeBoost(X_train, X_test, y_train, y_test, method='adaboost')
